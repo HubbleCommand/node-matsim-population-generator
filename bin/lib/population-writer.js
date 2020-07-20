ae68da23930c6c3b0faf64adaf36ea613f5bc933
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-07-20 12:01:53 +0200

    Code clean Population Writer

--- bin/lib/population-writer.js
+++ bin/lib/population-writer.js
@@ -21,30 +21,10 @@ function writePersonAndPlan(xmlRootEle, personId, mode, homeXY, workXY, workStar
     var plan = person.ele("plan");
 
     writeActivity(plan, "h", homeXY[0], homeXY[1], null, null);
-    /*var start = plan.ele("act")
-    start.att("type", "h")
-    start.att("x",homeXY[0])
-    start.att("y",homeXY[1])
-    //start.att("end_time", "07:00")*/
-
     writeLeg(plan, mode);
-
     writeActivity(plan, "w", homeXY[0], homeXY[1], workStart, workEnd);
-    /*var work = plan.ele("act")
-    work.att("type", "w")
-    work.att("x",workXY[0])
-    work.att("y",workXY[1] )
-    //work.att("duration", "08:00")
-    work.att("start_time", workStart);
-    work.att("end_time", workEnd);*/
-
     writeLeg(plan, mode);
-
     writeActivity(plan, "h", homeXY[0], homeXY[1], null, null);
-    /*var end = plan.ele("act")
-    end.att("type", "h")
-    end.att("x",homeXY[0])
-    end.att("y",homeXY[1])*/
 }
 
 exports.writePersonAndPlan = writePersonAndPlan

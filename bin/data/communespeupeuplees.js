4ab813aee47735cd0c5156e511a7f18550770693
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-07-20 11:01:24 +0200

    Create communespeupeuplees.js

---
+++ bin/data/communespeupeuplees.js
@@ -0,0 +1,16 @@
+var communes = {
+    "Nord": [
+        "Bellevue", "Céligny", "Collex-Bossy", "Genthod", "Pregny-Chambésy"
+    ],
+    "Ouest": [
+        "Aire-la-Ville", "Avully", "Avusy", "Cartigny", "Chancy", "Dardagny", "Laconnex", "Russin", "Satigny", "Soral"
+    ],
+    "Sud":[
+        "Bardonnex", "Confignon", "Perly-Certoux", "Troinex"
+    ],
+    "Est":[
+        "Anières", "Choulex", "Collonge-Bellerive", "Cologny", "Corsier", "Gy", "Hermance", "Jussy", "Meinier", "Presinge", "Puplinge", "Vandoeuvres"
+    ]
+};
+
+exports.communespeupeuplees = communes;
